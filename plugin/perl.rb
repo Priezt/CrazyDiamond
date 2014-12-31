3498ba52e991addfcc3ee2abe24322002284fa63
Priezt <vegetabird@gmail.com>
2014-12-30 23:02:44 -0800

    first commit

---
+++ plugin/perl.rb
@@ -0,0 +1,8 @@
+plugin {
+	match_command_line /^(perl|\S+\.pl)(\s|$)/
+	solution_for /\bCan't locate (\S+)\.pm in \@INC/ do
+		module_name = @m[1].gsub /\//, "::"
+		system("cpanm #{module_name}");
+	end
+}
+
