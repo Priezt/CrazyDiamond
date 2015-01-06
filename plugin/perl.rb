73431856f7a389702c766c78144415e8aecf2fd8
Priezt <vegetabird@gmail.com>
2015-01-05 18:58:58 -0800

    command stack; colorize output

--- plugin/perl.rb
+++ plugin/perl.rb
@@ -2,7 +2,7 @@ plugin {
 	match_command_line /^(perl|\S+\.pl)(\s|$)/
 	solution_for /\bCan't locate (\S+)\.pm in \@INC/ do
 		module_name = @m[1].gsub /\//, "::"
-		system("cpanm #{module_name}");
+		run("cpanm #{module_name}");
 	end
 }
 
